fa5a0871ebef0a3ad4b69fd828f873a91668122e
dhananjay sarkar <dhananjaysarkar156@gmail.com>
2020-07-18 10:50:58 +0530

    sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    SampleComponent
   ],
   imports: [
     BrowserModule,
